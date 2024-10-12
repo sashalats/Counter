3116202e84655181c2675410f0cf4bc94d3b2dd2
Sasha <sashalats@ya.ru>
2024-10-12 15:07:18 +0300

    Initial Commit

---
+++ CounterTests/CounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  CounterTests.swift
+//  CounterTests
+//
+//  Created by sashalats on 12.10.2024.
+//
+
+import Testing
+@testable import Counter
+
+struct CounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
